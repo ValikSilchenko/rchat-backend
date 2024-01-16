ca74fea595d6140ed591ccc681ee677d627e524b
ValikSilchenko <valik.silchenko@gmail.com>
2024-01-16 21:21:45 +0300

    feat: add app startup and shutdown events

--- rchat/app.py
+++ rchat/app.py
@@ -1,8 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
 
+from rchat.state import app_state
+from rchat.views.auth.views import router as auth_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
+
+
+@app.on_event("startup")
+async def startup():
+    await app_state.startup()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app_state.shutdown()
 
 
 if __name__ == "__main__":
