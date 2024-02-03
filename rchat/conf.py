12fbb59cd561f93c90e22fddb804e73611b9dbf4
Valik <valik.silchenko@gmail.com>
2024-02-03 12:23:37 +0300

    fix: fix backend url

--- rchat/conf.py
+++ rchat/conf.py
@@ -1,6 +1,4 @@
-SECRET_KEY = (
-    "10d4f2d93aed29b7be09a2bb0fe1109ede2a602505935063e3b423c272673eacf027d4bc329"
-    )
+SECRET_KEY = "10d4f2d93aed29b7be09a2bb0fe1109ede2a602505935063e3b423c272673eacf027d4bc329"
 # BASE_BACKEND_URL = "http://194.87.248.192:8000"
-BASE_BACKEND_URL = "http://localhost:8000"
-DATABASE_DSN = "postgresql://postgres:postgres@localhost:5431/postgres"
+BASE_BACKEND_URL = "http://26.20.186.152:8080"
+DATABASE_DSN = "postgresql://postgres:postgres@postgres/postgres"
