55606b9e8f240cd38f1bce8a5b5e5ea1a1e78f04
Valik <valik.silchenko@gmail.com>
2024-02-03 12:23:10 +0300

    feat: add log middleware

--- rchat/app.py
+++ rchat/app.py
@@ -1,12 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 
+from rchat.middlewares import access_log_middleware
 from rchat.state import app_state
 from rchat.views.auth.views import router as auth_router
+
+import logging
 
 app = FastAPI()
 
 app.include_router(auth_router)
+
+logging.basicConfig(level=logging.INFO)
 
 
 @app.on_event("startup")
@@ -19,11 +24,14 @@ async def shutdown():
     await app_state.shutdown()
 
 
+app.middleware("http")(access_log_middleware)
+
+
 if __name__ == "__main__":
     uvicorn.run(
         app="rchat.app:app",
         reload=True,
-        port=8000,
+        port=8080,
         host="0.0.0.0",
-        access_log=False
+        access_log=False,
     )
---
+++ rchat/middlewares.py
@@ -0,0 +1,19 @@
+from starlette.requests import Request
+from starlette.responses import Response
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+async def access_log_middleware(request: Request, call_next):
+    status = None
+    try:
+        response: Response = await call_next(request)
+        status = response.status_code
+        return response
+    except Exception:
+        status = 500
+        raise
+    finally:
+        logger.info("%s %s %s", status, request.method, request.url)
