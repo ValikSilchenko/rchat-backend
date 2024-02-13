54e7759113fb0cdc9e7b01a7112b9a7346b4ba1d
Valik <valik.silchenko@gmail.com>
2024-02-14 02:20:44 +0300

    feat: add logging setup

--- rchat/app.py
+++ rchat/app.py
@@ -1,29 +1,25 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from rchat.log import setup_logging
 from rchat.middlewares import access_log_middleware
 from rchat.state import app_state
 from rchat.views.auth.views import router as auth_router
 
-import logging
 
-app = FastAPI()
-
-app.include_router(auth_router)
-
-logging.basicConfig(level=logging.INFO)
-
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
     await app_state.startup()
-    await app_state.init_migrations()
-
-
-@app.on_event("shutdown")
-async def shutdown():
+    setup_logging()
+    yield
     await app_state.shutdown()
 
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
 
 app.middleware("http")(access_log_middleware)
 
