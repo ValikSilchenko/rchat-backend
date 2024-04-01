23a2f76755d1d673fac7036734d7e3d2cf9d2b76
Valik <valik.silchenko@gmail.com>
2024-04-01 20:09:39 +0300

    feat: add new tables

--- rchat/migrations/0001.initial.sql
+++ rchat/migrations/0001.initial.sql
@@ -1,5 +1,6 @@
-create table "photo" (
+create table "media" (
     id uuid primary key,
+    type varchar(16) not null,
     size_bytes bigint not null,
     extension varchar(16) not null,
     created_timestamp timestamp not null default now()
@@ -13,18 +14,30 @@ create table "user" (
     user_salt varchar(32) not null,
     first_name varchar(32),
     last_name varchar(32),
-    avatar_photo_id uuid references "photo" ("id"),
+    avatar_photo_id uuid references "media" ("id"),
     profile_status varchar(64),
     profile_bio varchar(512),
-    created_timestamp timestamp not null default (now() at time zone 'utc')
+    created_timestamp timestamp not null default now()
 );
+
+create table "user_settings" (
+    user_id uuid primary key references "user" ("id")
+);
+
+create table "user_contact" (
+    user_id uuid not null references "user" ("id"),
+    contact_user_id uuid not null references "user" ("id"),
+    created_timestamp timestamp not null default now()
+);
+
 create table "session" (
     id uuid primary key,
     user_id uuid not null references "user" ("id"),
     ip varchar(15),
     user_agent varchar(64),
     is_active bool not null default true,
-    created_timestamp timestamp not null default (now() at time zone 'utc')
+    device_fingerprint varchar(32) not null,
+    created_timestamp timestamp not null default now()
 );
 
 create table "geoip" (
@@ -39,23 +52,46 @@ create table "geoip" (
 create table "chat" (
     id uuid primary key,
     type varchar(16) not null,
-    avatar_photo_id uuid references "photo" ("id"),
     name varchar(32) not null,
+    avatar_photo_id uuid references "media" ("id"),
     description varchar(256),
     created_timestamp timestamp not null default now()
+);
+
+create table "chat_user" (
+    chat_id uuid references "chat" ("id"),
+    user_id uuid references "user" ("id"),
+    is_chat_owner bool not null default false,
+    last_available_message uuid references "message" ("id"),
+    created_timestamp timestamp not null default now(),
+    primary key ("chat_id", "user_id")
 );
 
 create table "message" (
     id uuid primary key,
     chat_id uuid not null references "chat" ("id"),
-    sender_user_id uuid not null references "user" ("id"),
+    sender_user_id uuid references "user" ("id"),
+    sender_chat_id uuid references "chat" ("id"),
     message_text varchar(4096),
-    audio_msg_file_id uuid,
-    video_msg_file_id uuid,
+    audio_msg_file_id uuid references "media" ("id"),
+    video_msg_file_id uuid references "media" ("id"),
     reply_to_message uuid references "message" ("id"),
     forwarded_message uuid references "message" ("id"),
     is_silent bool not null default false,
-    edited_at timestamp,
+    last_edited_at timestamp,
     created_timestamp timestamp not null default now()
+);
+
+create table "message_read" (
+    message_id uuid references "message" ("id"),
+    user_id uuid references "user" ("id"),
+    created_timestamp timestamp not null default now(),
+    primary key ("message_id", "user_id")
+);
+
+create table "message_attachment" (
+    message_id uuid references "message" ("id"),
+    media_id uuid references "media" ("id"),
+    primary key ("message_id", "message_id")
 );
 
