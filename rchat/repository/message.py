6148aac2978ac8b0d3531d560f6d5ac6601da731
Valik <valik.silchenko@gmail.com>
2024-04-14 12:38:50 +0300

    feat: add create and get messages repo methods

--- rchat/repository/message.py
+++ rchat/repository/message.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import Optional
 
 from asyncpg import Pool
 from pydantic import UUID4, UUID5, BaseModel
@@ -26,6 +27,9 @@ class MessageRepository:
         self._db = db
 
     async def create_message(self, message: MessageCreate) -> Message:
+        """
+        Добавляет сообщение в БД на основе модели для создания.
+        """
         sql_build = build_model(message)
         sql = f"""
             insert into "message" ({sql_build.field_names})
@@ -34,5 +38,37 @@ class MessageRepository:
         """
         async with self._db.acquire() as c:
             row = await c.fetchrow(sql, *sql_build.values)
+
+        return Message(**dict(row))
+
+    async def get_chat_messages(
+            self, chat_id: UUID4, last_order_id: int, limit: int
+    ) -> list[Message]:
+        """
+        Получает список сообщений чата отсортированных по дате создания.
+        """
+        sql = """
+            select * from "message"
+            where "chat_id" = $1 and "order_id" > $2
+            order by "created_timestamp" desc limit $3
+        """
+        async with self._db.acquire() as c:
+            rows = await c.fetchrow(sql, chat_id, last_order_id, limit)
+
+        return [Message(**dict(row)) for row in rows]
+
+    async def get_by_id(self, id_: UUID4) -> Optional[Message]:
+        """
+        Получает сообщение по его id.
+        """
+        sql = """
+            select * from "message"
+            where "id" = $1
+        """
+        async with self._db.acquire() as c:
+            row = await c.fetchrow(sql, id_)
+
+        if not row:
+            return
 
         return Message(**dict(row))
