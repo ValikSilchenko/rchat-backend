616b96fd1b7e20d2e7b50e939a38a3f91fb314c5
Valik <valik.silchenko@gmail.com>
2024-04-16 17:00:54 +0300

    feat: add get and update profile methods

--- rchat/repository/media.py
+++ rchat/repository/media.py
@@ -1,10 +1,27 @@
+from typing import Optional
+
 from asyncpg import Pool
 from pydantic import UUID4
+
+from rchat.schemas.models import Media
 
 
 class MediaRepository:
     def __init__(self, db: Pool):
         self._db = db
+
+    async def get_media_by_id(self, id_: UUID4) -> Optional[Media]:
+        sql = """
+            select * from "media"
+            where "id" = $1
+        """
+        async with self._db.acquire() as c:
+            row = await c.fetchrow(sql, id_)
+
+        if not row:
+            return
+
+        return Media(**dict(row))
 
     def get_media_url(self, id_: UUID4):
         return ""
--- rchat/views/user/models.py
+++ rchat/views/user/models.py
@@ -1,7 +1,36 @@
-from pydantic import BaseModel
+from enum import StrEnum
+
+from pydantic import UUID4, BaseModel, Field
 
 from rchat.repository.user import UserFind
+from rchat.views.auth.models import UserDataPatternEnum
 
 
 class FindUsersResponse(BaseModel):
     users: list[UserFind]
+
+
+class ProfileResponse(BaseModel):
+    public_id: str
+    email: str
+    first_name: str
+    last_name: str | None
+    avatar_photo_url: str | None
+    profile_status: str | None
+    profile_bio: str | None
+
+
+class ProfileUpdateStatusEnum(StrEnum):
+    public_id_already_taken = "public_id_already_taken"
+    invalid_photo_id = "invalid_photo_id"
+
+
+class ProfileUpdateBody(BaseModel):
+    public_id: str = Field(
+        min_length=4, max_length=32, pattern=UserDataPatternEnum.public_id
+    )
+    first_name: str = Field(min_length=2, max_length=32)
+    last_name: str | None = Field(min_length=2, max_length=32)
+    avatar_photo_id: UUID4 | None
+    profile_status: str | None = Field(max_length=64)
+    profile_bio: str | None = Field(max_length=512)
