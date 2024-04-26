41492fd75baec985b11b06f348c48fa79aceb111
ValikSilchenko <valik.silchenko@gmail.com>
2024-04-26 07:16:34 +0300

    feat: add data migration script

--- rchat/migrations/0002.add_action_messages.rollback.sql
+++ rchat/migrations/0002.add_action_messages.rollback.sql
@@ -1,2 +1,11 @@
+alter table "chat" add column "created_by" uuid references "user" ("id");
+
+with t as (
+    select "user_initiated_action", "chat_id" from "message"
+    where type = 'created_chat'
+)
+update "chat" set "created_by" = t."user_initiated_action"
+from t where t."chat_id" = "chat"."id";
+
 alter table "message" drop column user_initiated_action;
 alter table "message" drop column user_involved;
--- rchat/migrations/0002.add_action_messages.sql
+++ rchat/migrations/0002.add_action_messages.sql
@@ -1,2 +1,11 @@
 alter table "message" add column user_initiated_action uuid references "user" ("id");
 alter table "message" add column user_involved uuid references "user" ("id");
+
+with t as (
+    select "created_by", "id" from "chat"
+    where "created_by" is not null
+)
+update "message" set "user_initiated_action" = t."created_by"
+from t where t."id" = "message"."chat_id" and type = 'created_chat';
+
+alter table "chat" drop column "created_by";
